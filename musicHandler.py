a097af829f48cdfd47e89ae944c392291ea502bf
cstuart1310 <cstuart1310@gmail.com>
2023-02-13 22:13:56 +0000

    Downloads audio and video seperately

--- musicHandler.py
+++ musicHandler.py
@@ -4,14 +4,24 @@ from pytube import YouTube
 from pytube import Playlist 
 import urllib
 import time
+import subprocess
+
+headerRows=1 #no of header/title rows to skip
+
+#Filetype, incl audio, incl video
+fileTypes={
+    "mp3":"A",
+    "mp4":"AV"
+}
 
 def readFiles():
     with open('downloads.csv', 'r') as file:
         reader = csv.reader(file,delimiter=",")
-        rowCount=0
+        rowCount=1
         for row in reader:
+            if rowCount>headerRows:#only begins after skipping headers
+                downloadRow(row)
             rowCount=rowCount+1
-            downloadRow(row)
 
 def downloadRow(row):
     
@@ -25,17 +35,51 @@ def downloadRow(row):
     print("outputDir:",row[2])
     print("Playlist:",row[3])
 
+    fileTypeInfo=getFileTypeInfo(fileType)
+
     print("\nDownloading Playlist")
-    if ("https://") in row[3]:
-        try:
-            for video in playlist.videos:
-                if fileType=="mp3":
-                    video.streams.get_audio_only().download()
-                #video.streams.first().download()
-        except urllib.error.HTTPError:
-            print("Error, sleeping")
-            time.sleep(30)
+    if ("https://") in row[3]:#Makes sure url is a url
+        for video in playlist.videos:
+            vidTitle=YouTube(video).title
+            print(vidTitle)
+            if "A" in fileTypeInfo:
+                getHighestAudio(video)
+            if "V" in fileTypeInfo:
+                getHighestVideo(video)
         print("_"*20)
+    else:
+        print ("Error, url is either not a url or isn't pytube-able")
+
+def getHighestAudio(video,outputDir):#Downloads the highest quality audio available
+    print("Downloading Audio")
+    downloadSuccessful=False
+    while downloadSuccessful==False:
+        try:
+            video.streams.get_audio_only().download(outputDir)
+            downloadSuccessful=True                        
+        except urllib.error.HTTPError:#If url isn't pytube compatible
+                print("Error, sleeping")
+                time.sleep(30)
+
+def getHighestVideo(video,outputDir):#Downloads the highest quality video available
+    print("Downloading Video")
+    downloadSuccessful=False
+    while downloadSuccessful==False:
+        try:
+            video.streams.get_highest_resolution().download(outputDir)
+            downloadSuccessful=True                        
+        except urllib.error.HTTPError:#If url isn't pytube compatible
+                print("Error, sleeping")
+                time.sleep(30)
+    
+def convertToFileType(vidTitle,fileType):
+    file=vidTitle+".mp4"#may need to futureproof if non-mp4 streams get downloaded from youtube
+    print(subprocess.run('ffmpeg -i ',file,".",fileType,',shell=True,capture_output=True'))
+
+def getFileTypeInfo(fileType):
+    fileTypeChannels=(fileTypes[fileType])
+    return fileTypeChannels
+
 
 def expBackOff():
     multiplier=1
