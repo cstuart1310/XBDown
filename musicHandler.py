f04138963de98a6a0b979aa74a65a28812d72a0d
cstuart1310 <cstuart1310@gmail.com>
2023-02-13 20:40:18 +0000

    mp3 test

--- musicHandler.py
+++ musicHandler.py
@@ -2,6 +2,8 @@ import os
 import csv
 from pytube import YouTube
 from pytube import Playlist 
+import urllib
+import time
 
 def readFiles():
     with open('downloads.csv', 'r') as file:
@@ -12,25 +14,31 @@ def readFiles():
             downloadRow(row)
 
 def downloadRow(row):
-    try:
-        name=row[0]
-        fileType=row[1]
-        outputDir=row[2]
-        playlist=Playlist(str(row[3]))
+    
+    name=row[0]
+    fileType=row[1]
+    outputDir=row[2]
+    playlist=Playlist(str(row[3]))
 
-        print("Name:",row[0])
-        print("fileType:",row[1])
-        print("outputDir:",row[2])
-        print("Playlist:",row[3])
+    print("Name:",row[0])
+    print("fileType:",row[1])
+    print("outputDir:",row[2])
+    print("Playlist:",row[3])
 
-        print("\nDownloading Playlist")
+    print("\nDownloading Playlist")
+    if ("https://") in row[3]:
+        try:
+            for video in playlist.videos:
+                if fileType=="mp3":
+                    video.streams.get_audio_only().download()
+                #video.streams.first().download()
+        except urllib.error.HTTPError:
+            print("Error, sleeping")
+            time.sleep(30)
+        print("_"*20)
 
-        for video in playlist.videos:
-            print("1")
-            video.streams.first().download()
-    except:
-        print("shit")
-    print("_"*20)
-
-
+def expBackOff():
+    multiplier=1
+    delay=10
+    print("Delaying for ",delay*multiplier,"seconds")
 readFiles()
