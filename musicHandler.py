2bcc7ffc015be0566d0b9bcdce851c81b9e19902
cstuart1310 <cstuart1310@gmail.com>
2023-02-13 22:27:15 +0000

    Improved looks

--- musicHandler.py
+++ musicHandler.py
@@ -30,51 +30,58 @@ def downloadRow(row):
     outputDir=row[2]
     playlist=Playlist(str(row[3]))
 
+    
+    fileTypeInfo=getFileTypeInfo(fileType)
+
     print("Name:",row[0])
     print("fileType:",row[1])
     print("outputDir:",row[2])
     print("Playlist:",row[3])
 
-    fileTypeInfo=getFileTypeInfo(fileType)
 
-    print("\nDownloading Playlist")
+    print("\nDownloading Playlist",name)
     if ("https://") in row[3]:#Makes sure url is a url
-        for video in playlist.videos:
-            vidTitle=YouTube(video).title
+        playlistVideos=playlist.videos
+        print("Playlist contains",len(playlistVideos),"videos")
+        for video in playlistVideos:
+            print(video)
+            vidTitle=str(video.title)
             print(vidTitle)
             if "A" in fileTypeInfo:
                 getHighestAudio(video)
             if "V" in fileTypeInfo:
                 getHighestVideo(video)
+            convertToFileType(vidTitle,fileType)
         print("_"*20)
     else:
         print ("Error, url is either not a url or isn't pytube-able")
 
-def getHighestAudio(video,outputDir):#Downloads the highest quality audio available
+def getHighestAudio(video):#Downloads the highest quality audio available
     print("Downloading Audio")
     downloadSuccessful=False
     while downloadSuccessful==False:
         try:
-            video.streams.get_audio_only().download(outputDir)
+            video.streams.get_audio_only().download()
             downloadSuccessful=True                        
         except urllib.error.HTTPError:#If url isn't pytube compatible
                 print("Error, sleeping")
                 time.sleep(30)
 
-def getHighestVideo(video,outputDir):#Downloads the highest quality video available
+def getHighestVideo(video):#Downloads the highest quality video available
     print("Downloading Video")
     downloadSuccessful=False
     while downloadSuccessful==False:
         try:
-            video.streams.get_highest_resolution().download(outputDir)
+            video.streams.get_highest_resolution().download()
             downloadSuccessful=True                        
         except urllib.error.HTTPError:#If url isn't pytube compatible
                 print("Error, sleeping")
                 time.sleep(30)
     
 def convertToFileType(vidTitle,fileType):
-    file=vidTitle+".mp4"#may need to futureproof if non-mp4 streams get downloaded from youtube
-    print(subprocess.run('ffmpeg -i ',file,".",fileType,',shell=True,capture_output=True'))
+    print("Converting to",fileType)
+    fileName=vidTitle+".mp4"#may need to futureproof if non-mp4 streams get downloaded from youtube
+    subprocess.run('ffmpeg -i ',fileName,".",fileType,',shell=True,capture_output=True')
 
 def getFileTypeInfo(fileType):
     fileTypeChannels=(fileTypes[fileType])
